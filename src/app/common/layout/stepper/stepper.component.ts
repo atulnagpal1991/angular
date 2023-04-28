0b3bd1625f4d739bbb9b00f332b533e5ff54d564
atulnagpal1991 <atulnagpal1991@gmail.com>
2023-04-28 18:25:32 +0530

    add code

--- src/app/common/layout/stepper/stepper.component.ts
+++ src/app/common/layout/stepper/stepper.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SelectEvent } from "@progress/kendo-angular-layout";
 
 @Component({
   selector: 'app-stepper',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./stepper.component.css']
 })
 export class StepperComponent {
+  public onTabSelect(e: SelectEvent): void {
+    console.log(e);
+  }
   public current = 1;
 
   public steps = [
@@ -15,4 +19,80 @@ export class StepperComponent {
     { label: "Preview", icon: "eye" },
     { label: "Submit", icon: "file-add" },
   ];
+
+
+
+  code = `
+  import { Component } from '@angular/core';
+
+  @Component({
+      selector: 'my-app',
+      template: "
+          <kendo-stepper
+              [steps]="steps"
+              stepType="full"
+              [(currentStep)]="current"
+              [linear]="false"
+              [style.width.px]="570">
+          </kendo-stepper>
+      "
+  })
+  export class AppComponent {
+      public current = 1;
+  
+      public steps = [
+          { label: 'Personal Info', icon: 'user' },
+          { label: 'Education', icon: 'book' },
+          { label: 'Attachments', icon: 'paperclip', optional : true },
+          { label: 'Preview', icon: 'eye' },
+          { label: 'Submit', icon: 'file-add' }
+      ];
+  }
+`
+
+
+code1 = `
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { FormsModule } from '@angular/forms';
+import { IconsModule } from '@progress/kendo-angular-icons';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    imports:      [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        LayoutModule,
+        IconsModule,
+        InputsModule,
+        LabelModule,
+        ButtonsModule,
+        DropDownsModule
+    ],
+    declarations: [AppComponent],
+    bootstrap:    [AppComponent]
+})
+export class AppModule {}
+`
+
+
+code2 = `
+import './polyfills';
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+enableProdMode();
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
+`
+
 }
