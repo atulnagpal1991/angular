3bc8cf05b5219f1bd5630ae3503ca073c0dc4585
atulnagpal1991 <atulnagpal1991@gmail.com>
2023-06-02 20:13:03 +0530

    fixes

--- src/app/common/layout/progress/progress.component.ts
+++ src/app/common/layout/progress/progress.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SelectEvent } from "@progress/kendo-angular-layout";
+import { ProgressBarAnimation } from "@progress/kendo-angular-progressbar";
 
 @Component({
   selector: 'app-progress',
@@ -11,6 +12,18 @@ export class ProgressComponent {
     console.log(e);
   }
 
-  public value = 68;
+  public value1 = 68;
   public chunks = 5;
+
+  public animation: ProgressBarAnimation = {
+    duration: 2000,
+  };
+  public value = 0;
+  public get text(): string {
+    return this.value === 0 ? "Start" : "Reset";
+  }
+
+  public changeProgress(): void {
+    this.value = this.value === 0 ? 68 : 0;
+  }
 }
