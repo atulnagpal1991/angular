fd43f04fcc0512c750eafba46eb13187458995d8
atulnagpal1991 <atulnagpal1991@gmail.com>
2023-06-04 20:16:44 +0530

    fixes

--- src/app/common/layout/accordion/accordion.component.ts
+++ src/app/common/layout/accordion/accordion.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SelectEvent } from "@progress/kendo-angular-layout";
+import { PanelBarExpandMode } from "@progress/kendo-angular-layout";
 
 @Component({
   selector: 'app-accordion',
@@ -10,6 +11,8 @@ export class AccordionComponent {
   public onTabSelect(e: SelectEvent): void {
     console.log(e);
   }
+
+  public expandMode: PanelBarExpandMode = 1;
 
   private baseImageUrl =
   "https://demos.telerik.com/kendo-ui/content/web/panelbar/";
