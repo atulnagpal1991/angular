ae7a904779b6a18f7dde57a03d56ba097f68677f
atulnagpal1991 <atulnagpal1991@gmail.com>
2023-06-14 16:13:12 +0530

    fixes

--- src/app/common/layout/accordion/accordion.component.ts
+++ src/app/common/layout/accordion/accordion.component.ts
@@ -179,26 +179,24 @@ export class AccordionComponent {
   `
 
   code1 = `
-import { Component } from '@angular/core';
-import { SelectEvent } from "@progress/kendo-angular-layout";
-import { PanelBarExpandMode } from "@progress/kendo-angular-layout";
-@Component({
-selector: 'app-accordion',
-templateUrl: './accordion.component.html',
-styleUrls: ['./accordion.component.css']
-})
-export class AccordionComponent {
-public onTabSelect(e: SelectEvent): void {
-console.log(e);
-}
-public expandMode: PanelBarExpandMode = 1;
-private baseImageUrl =
-"https://demos.telerik.com/kendo-ui/content/web/panelbar/";
-public imageUrl(imageName: string): string {
-return this.baseImageUrl + imageName + ".jpg";
-}
-}
- 
-`
-
+  import { Component } from '@angular/core';
+  import { SelectEvent } from "@progress/kendo-angular-layout";
+  import { PanelBarExpandMode } from "@progress/kendo-angular-layout";
+  @Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.css']
+  })
+  export class AccordionComponent {
+    public onTabSelect(e: SelectEvent): void {
+      console.log(e);
+    }
+    public expandMode: PanelBarExpandMode = 1;
+    private baseImageUrl =
+    "https://demos.telerik.com/kendo-ui/content/web/panelbar/";
+    public imageUrl(imageName: string): string {
+      return this.baseImageUrl + imageName + ".jpg";
+    }
+  }
+  `
 }
--- src/app/common/layout/buttons/buttons.component.ts
+++ src/app/common/layout/buttons/buttons.component.ts
@@ -277,121 +277,122 @@ export class ButtonsComponent {
 
   code1 = `
   import { Component, ViewEncapsulation } from '@angular/core';
-import { ChipRemoveEvent } from "@progress/kendo-angular-buttons";
-import { SelectEvent } from "@progress/kendo-angular-layout";
+  import { ChipRemoveEvent } from "@progress/kendo-angular-buttons";
+  import { SelectEvent } from "@progress/kendo-angular-layout";
 
-@Component({
-  selector: 'app-buttons',
-  templateUrl: './buttons.component.html',
-  styleUrls: ['./buttons.component.css'],
-  encapsulation: ViewEncapsulation.None
-})
-export class ButtonsComponent {
-  public onTabSelect(e: SelectEvent): void {
-    console.log(e);
-  }
-  public current = 1;
-
-  public imageUrl =
-    "https://demos.telerik.com/kendo-ui/content/shared/icons/sports/snowboarding.png";
-
-  public fabItems = [
-    { icon: "google", label: "Google" },
-    { icon: "reddit", label: "Reddit" },
-    { icon: "dribbble", label: "Dribbble" },
-  ];
-
-  public splitButtonItems = [
-    {
-      text: "Keep Text Only",
-      icon: "clipboard-text",
-      click: (): void => {
-        console.log("Keep Text Only click handler");
-      },
-    },
-    {
-      text: "Paste as HTML",
-      icon: "clipboard-code",
-    },
-    {
-      text: "Paste Markdown",
-      icon: "clipboard-markdown",
-    },
-    {
-      text: "Set Default Paste",
-    },
-  ];
-
-  public dropDownButtonItems = [
-    {
-      text: "My Profile",
-    },
-    {
-      text: "Friend Requests",
-    },
-    {
-      text: "Account Settings",
-    },
-    {
-      text: "Support",
-    },
-    {
-      text: "Log Out",
-    },
-  ];
-
-  public chips = [
-    {
-      label: "Pedro Afonso",
-      iconClass: "k-chip-avatar pedro",
-      selected: false,
-      removable: true,
-      removeIcon: " ",
-    },
-    {
-      label: "Thomas Hardy",
-      iconClass: "k-chip-avatar thomas",
-      selected: false,
-      removable: true,
-      removeIcon: "k-icon k-i-trash",
-    },
-    {
-      label: "Christina Berg",
-      iconClass: "k-chip-avatar christina",
-      selected: false,
-      removable: true,
-      removeIcon: "k-icon k-i-x-outline",
-    },
-    {
-      label: "Paula Wilson",
-      iconClass: "k-chip-avatar paula",
-      selected: false,
-      removable: true,
-      removeIcon: "k-icon k-i-x",
-    },
-  ];
-
-  public onRemove(e: ChipRemoveEvent): void {
-    const index = this.chips.findIndex((c) => c.label === e.sender.label);
-    this.chips.splice(index, 1);
-  }
-
-  public onFabClick(): void {
-    console.log("Added");
-  }
-
-  public onSplitButtonClick(): void {
-    console.log("Paste");
-  }
-
-  public onSplitButtonItemClick(dataItem: { [key: string]: unknown }): void {
-    if (dataItem) {
-      console.log(dataItem['text']);
+  @Component({
+    selector: 'app-buttons',
+    templateUrl: './buttons.component.html',
+    styleUrls: ['./buttons.component.css'],
+    encapsulation: ViewEncapsulation.None
+  })
+  export class ButtonsComponent {
+    public onTabSelect(e: SelectEvent): void {
+      console.log(e);
     }
-  }
+    public current = 1;
 
-  public onButtonClick(): void {
-    console.log("click");
+    public imageUrl =
+      "https://demos.telerik.com/kendo-ui/content/shared/icons/sports/snowboarding.png";
+
+    public fabItems = [
+      { icon: "google", label: "Google" },
+      { icon: "reddit", label: "Reddit" },
+      { icon: "dribbble", label: "Dribbble" },
+    ];
+
+    public splitButtonItems = [
+      {
+        text: "Keep Text Only",
+        icon: "clipboard-text",
+        click: (): void => {
+          console.log("Keep Text Only click handler");
+        },
+      },
+      {
+        text: "Paste as HTML",
+        icon: "clipboard-code",
+      },
+      {
+        text: "Paste Markdown",
+        icon: "clipboard-markdown",
+      },
+      {
+        text: "Set Default Paste",
+      },
+    ];
+
+    public dropDownButtonItems = [
+      {
+        text: "My Profile",
+      },
+      {
+        text: "Friend Requests",
+      },
+      {
+        text: "Account Settings",
+      },
+      {
+        text: "Support",
+      },
+      {
+        text: "Log Out",
+      },
+    ];
+
+    public chips = [
+      {
+        label: "Pedro Afonso",
+        iconClass: "k-chip-avatar pedro",
+        selected: false,
+        removable: true,
+        removeIcon: " ",
+      },
+      {
+        label: "Thomas Hardy",
+        iconClass: "k-chip-avatar thomas",
+        selected: false,
+        removable: true,
+        removeIcon: "k-icon k-i-trash",
+      },
+      {
+        label: "Christina Berg",
+        iconClass: "k-chip-avatar christina",
+        selected: false,
+        removable: true,
+        removeIcon: "k-icon k-i-x-outline",
+      },
+      {
+        label: "Paula Wilson",
+        iconClass: "k-chip-avatar paula",
+        selected: false,
+        removable: true,
+        removeIcon: "k-icon k-i-x",
+      },
+    ];
+
+    public onRemove(e: ChipRemoveEvent): void {
+      const index = this.chips.findIndex((c) => c.label === e.sender.label);
+      this.chips.splice(index, 1);
+    }
+
+    public onFabClick(): void {
+      console.log("Added");
+    }
+
+    public onSplitButtonClick(): void {
+      console.log("Paste");
+    }
+
+    public onSplitButtonItemClick(dataItem: { [key: string]: unknown }): void {
+      if (dataItem) {
+        console.log(dataItem['text']);
+      }
+    }
+
+    public onButtonClick(): void {
+      console.log("click");
+    }
   }
   `
 
